685970c415a26afcff08b2e800cfabe808eaad6c
Chris Staunton <stauntch@tcd.ie>
2021-01-28 18:09:04 +0000

    added apikey and gitignore

--- trading.py
+++ trading.py
@@ -1,2 +1,17 @@
-print("Hello")
-print("STONKSSSSSSSSSSSSSSSSSSSS")
+import requests
+import os
+import dotenv
+dotenv.load_dotenv(verbose=True)
+key = os.getenv('VantageAlphaApi')
+#print(key)
+
+# key = 
+function = 'TIME_SERIES_INTRADAY'
+sym = 'IBM'
+interval = '5min'
+link = 'https://www.alphavantage.co/query?function=' + function + '&symbol=' + sym + '&interval=' + interval + '&apikey=' + key
+
+req = requests.get(link)
+
+print(req.json())
+
